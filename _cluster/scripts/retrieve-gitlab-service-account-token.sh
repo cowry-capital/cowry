11b612f30ccc97888813179f7dc5b93a48bdc0c1
Obinna Nnodim <obiknows88@gmail.com>
2019-12-21 10:25:01 -0800

    blank slate

--- _cluster/scripts/retrieve-gitlab-service-account-token.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Retrieve the token for the gitlab-admin service account:
-kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep gitlab-admin | awk '{print $1}')
