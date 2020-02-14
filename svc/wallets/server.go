16f3ecbae54bd7fb3ae2e7ae1cd2482e8547e1c8
Obinna Nnodim <obiknows88@gmail.com>
2020-02-14 07:08:53 -0700

    started working on the wallets svc

---
+++ svc/wallets/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+  e := echo.New()
+  
+  // Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Route => handler
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!\n")
+	})
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+}
