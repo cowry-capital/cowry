ecfca31114b75a673f6b8b98f997f846143e49fc
Obinna Nnodim <obiknows88@gmail.com>
2020-02-22 02:29:48 -0700

    added in default test typescript firebase cloud functions env for following along fireship.io course

---
+++ firebase/README.md
@@ -0,0 +1,12 @@
+COWRY's FIREBASE
+================
+
+note: must use node version 8
+
+```
+functions/ - backend
+public/ - frontend
+```
+
+
+HTTP functions invoked manually (via REQUESTS)
---
+++ firebase/functions/src/http.ts
@@ -0,0 +1,18 @@
+import * as functions from "firebase-functions";
+
+import * as admin from "firebase-admin";
+// THIS IS FOR ACCESS TO FIRESTORE AS ADMIN FROM FUNCTION
+admin.initializeApp();
+
+export const basicHTTP = functions.https.onRequest((req,res) => {
+  
+  // get req info from (body, query, headers, etc.)
+  const name = req.query.name
+  
+  if(!name) {
+    res.status(400).send('ERROR - Gotta add a name as query param')
+  } else {
+    res.send(`What's good from COWRY - ${name}`)
+  }
+  
+})
